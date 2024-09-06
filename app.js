e8c7ca882196a29da659da6a833e6964bf35667e
Maddy Foley <maddyfoley5@gmail.com>
2024-09-06 15:53:27 -0400

    set up sqlite3

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
+import PDFDocument from 'pdfkit';
+import fs from 'fs';
+import sqlite3 from 'sqlite3';
+import { makeDatabase } from './db.js';
+
+
+const db = new sqlite3.Database(':memory:');
+
+makeDatabase(db);
+
 
 const doc = new PDFDocument;
 
