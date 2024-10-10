653a63cf42d55bf1048071ad9109d3904bc80c99
Maddy Foley <maddyfoley5@gmail.com>
2024-10-09 22:30:23 -0400

    fixed typo

--- src/main/java/com/example/job_tailor/JobTailorApplication.java
+++ src/main/java/com/example/job_tailor/JobTailorApplication.java
@@ -1,14 +1,12 @@
-package com.example.job_tailer;
+package com.example.job_tailor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobTailerApplication {
-
+public class JobTailorApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-//		SpringApplication.run(JobTailerApplication.class, args);
+		SpringApplication.run(JobTailorApplication.class, args);
 	}
 
 }
