33e910357f699a6ce7708f430fb81589b1b6cb31
Maddy Foley <maddyfoley5@gmail.com>
2024-10-11 16:50:11 -0400

    fixed pom files, dockerized backend and DB

--- backend/src/main/java/com/example/job_tailor/JobTailorApplication.java
+++ backend/src/main/java/com/example/job_tailor/JobTailorApplication.java
@@ -9,3 +9,4 @@ public class JobTailorApplication {
         SpringApplication.run(JobTailorApplication.class, args);
     }
 }
+
---
+++ backend/src/test/java/com/example/job_tailor/JobTailorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_tailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTailorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
