7170e1a24cc4a17cc1b4132d9370c29b875ecd30
Maddy Foley <maddyfoley5@gmail.com>
2024-10-27 21:14:17 -0400

    added skill get controller

--- backend/src/main/java/com/example/job_tailor/controller/CandidateController.java
+++ backend/src/main/java/com/example/job_tailor/controller/CandidateController.java
@@ -45,6 +45,4 @@ public class CandidateController {
                         .body(createdCandidate);
             }
     }
-
-
 }
--- backend/src/main/java/com/example/job_tailor/service/SkillService.java
+++ backend/src/main/java/com/example/job_tailor/service/SkillService.java
@@ -2,7 +2,9 @@ package com.example.job_tailor.service;
 
 import com.example.job_tailor.model.Skill;
 
+import java.util.List;
+
 public interface SkillService {
     Skill createSkill(Skill skill);
-//    Skill getSkill();
+    List<Skill> getSkills();
 }
