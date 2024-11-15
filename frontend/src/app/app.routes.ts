552051bb011e20cba9eb6055ca23154920ea77f1
Maddy Foley <maddyfoley5@gmail.com>
2024-11-14 19:46:44 -0500

    testing components and routers

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'candidate', component: UserComponent}
+ ];
